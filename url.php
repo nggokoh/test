1d694dd1eebf42ce598a89eccc7f5107a538b68f
mas ngo <nggokoh@gmail.com>
2025-05-27 11:04:03 +0700

    Create url.php

---
+++ url.php
@@ -0,0 +1,14 @@
+!DOCTYPE html>
+<html>
+  <head>
+    <title>Sistem Dalam Perbaikan</title>
+  </head>
+  <body>
+    <main>
+      <h1>PERBAIKAN JALAN</h1>
+      <h2>Gudangnya PERBAIAKAN JALAN</h2>
+      <p>Mohon tunggu sebentar.</p>
+      <img src="logo_dicoding.png" alt="Image dicoding" />
+    </main>
+  </body>
+</html>
